d70d8ab512b1aba976ffb1562931fb67e96e71f0
ethanryan <ethanryan@gmail.com>
2017-04-11 12:13:51 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
+
+  #songs
+  get "/songs", to: "songs#index"
+  get "/songs/new", to: "songs#new", as: "new_song"
+  post "/songs", to: "songs#create"
+  get "/songs/:id", to: "songs#show", as: "song"
+  get "/songs/:id/edit", to: "songs#edit", as: "edit_song"
+  patch "/songs/:id", to: "songs#update"
+
+  #artists
+  get "/artists", to: "artists#index"
+  get "/artists/new", to: "artists#new", as: "new_artist"
+  post "/artists", to: "artists#create"
+  get "/artists/:id", to: "artists#show", as: "artist"
+  get "/artists/:id/edit", to: "artists#edit", as: "edit_artist"
+  patch "/artists/:id", to: "artists#update"
+
+  #genres
+  get "/genres", to: "genres#index"
+  get "/genres/new", to: "genres#new", as: "new_genre"
+  post "/genres", to: "genres#create"
+  get "/genres/:id", to: "genres#show" , as: "genre"
+  get "/genres/:id/edit", to: "genres#edit" , as: "edit_genre"
+  patch "/genres/:id", to: "genres#update"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
